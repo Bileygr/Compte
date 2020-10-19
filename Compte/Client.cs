d500a055f943e8bb6bea04941147c0cf0c482a20
Bileygr <cheiksiramakankeita@gmail.com>
2020-10-19 17:42:11 +0200

    Commit initial.

---
+++ Compte/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compte
+{
+    class Client
+    {
+        private string nom;
+        private string prenom;
+        private string telephone;
+
+        public Client(string nom, string prenom, string telephone)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.telephone = telephone;
+        }
+
+        public string Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value; }
+        }
+
+        public string Prenom
+        {
+            get { return this.prenom; }
+            set { this.prenom = value; }
+        }
+
+        public string Telephone
+        {
+            get { return this.telephone; }
+            set { this.telephone = value; }
+        }
+    }
+}
---
+++ Compte/Compte.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Compte
+{
+    class Compte
+    {
+        private int numero;
+        private double solde;
+        private Client client;
+        private [] mouvement;
+
+        public Compte()
+        {
+
+        }
+     
+        public int Numero
+        {
+            get { return this.numero; }
+            set { this.numero = value:}
+        }
+
+        public Client Client
+        {
+            get { return this.client; }
+            set { this.client = value; }
+        }
+
+        public Array Mouvement
+        {
+            get { return this.mouvement; }
+            set { }
+        }
+    }
+}
